bc7aabeb455bc0af847e94bd2c8e0c94446c564d
ihtisham Javed <ihtishamjaved9@gmail.com>
2019-12-12 23:36:51 +0500

    lt

--- common/models/message.js
+++ common/models/message.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = function(message) {
-message.updateReadStatus = function(id,userId,chatUserId, callback) {
-  message.updateAll({and:[{id:{lte:id}},{for:userId},{from:chatUserId}]},{read:1}).then(async function(res){
+message.updateReadStatus = function(id,userId,chatUserId,serviceId, callback) {
+  message.updateAll({and:[{id:{lte:id}},{for:userId},{from:chatUserId},{serviceId:serviceId}]},{read:1}).then(async function(res){
   	callback(null, {"statusCode":200,message:"successfully updated read status"});
   });
 };
