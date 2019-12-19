0d5f6fd6ca967667525aa7494dd9439ba3d7d3c4
ihtisham Javed <ihtishamjaved9@gmail.com>
2019-12-19 23:50:09 +0500

    lt

--- server/server.js
+++ server/server.js
@@ -42,12 +42,12 @@ boot(app, __dirname, function(err) {
       user.find({where:{mobile:data.phoneNumber_from}}).then(async function(userFound){
         if(userFound.length){
           console.log("inside if");
-          await msg.create({text:data.message,createdAt:current_date_time,from:userFound[0].id,read:0});
+          await msg.create({text:data.message,createdAt:data.current_date_time,from:userFound[0].id,read:0});
           await msg.broadcast.emit('fMessageFromRing', data);
           
         }else{
           await user.create({mobile:data.phoneNumber_from,fname:data.fname,lname:data.lname});
-          await msg.create({text:data.message,createdAt:current_date_time,from:userFound[0].id,read:0});
+          await msg.create({text:data.message,createdAt:data.current_date_time,from:userFound[0].id,read:0});
           await socket.broadcast.emit('fMessageFromRing', data);
         }
         console.log("new message from stephen",data);
