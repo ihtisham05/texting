b213359aaed32fb1f0ce71ed0e888eaad0c28aa4
ihtisham Javed <ihtishamjaved9@gmail.com>
2019-12-24 22:23:01 +0500

    lt

--- common/models/customuser.js
+++ common/models/customuser.js
@@ -4,7 +4,7 @@ module.exports = function(customUser) {
 customUser.searchRingCentralUser = function(name, callback) {
 	customUser.find({
 	  limit: 30,
-	  where:{searchName:{like: '%'+ name +'%'}}}).then(async function(users){
+	  where:{and:[{searchName:{like: '%'+ name +'%'}},{type:"ringCentral"}]}}).then(async function(users){
 	  	callback(null,users);
 	  });
 };
